f8240084590bd7366022d46700ce641bbca99733
Marc <35142431+mdickens@users.noreply.github.com>
2025-06-08 20:10:49 -0500

    publish website with latest updates

--- docs/script.v2.js
+++ docs/script.v2.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
         lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
     });
 
-	const scrollableContainer = document.getElementById('main');
+	const scrollableContainer = document.getElementById('site-wrapper');
 	if (scrollableContainer) {
     	scrollableContainer.addEventListener('scroll', function() {
         console.log("Inner container scrolled!");
