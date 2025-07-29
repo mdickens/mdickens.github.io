f7be60fc0aac06f3fb374d40aa9bf19231bc6124
Marc Dickenson <35142431+mdickens@users.noreply.github.com>
2025-07-28 20:54:44 -0500

    publish website with latest updates

--- docs/projects/chess.vibe.coding.session.18/copy.to.dl.sh
+++ docs/projects/chess.vibe.coding.session.18/copy.to.dl.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -x 
-rm -rf ~/Downloads/*.mp3 ~/Downloads/index.html  ~/Downloads/tests ~/Downloads/script.js ~/Downloads/style.css ~/Downloads/script.js; cp -p -f -v -r * ~/Downloads  
+#rm -rf ~/Downloads/*.mp3 ~/Downloads/index.html  ~/Downloads/tests ~/Downloads/script.js ~/Downloads/style.css ~/Downloads/script.js; cp -p -f -v -r *.html *.js *.mp3 *.json *.css tests ~/Downloads  
+rm -rf ~/Downloads/chess
+mkdir ~/Downloads/chess
+cp -p -f -v -r * ~/Downloads/chess
